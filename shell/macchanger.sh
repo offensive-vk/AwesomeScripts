6fddcade842ad297753b4020a9d1f797e19deec9
Vedansh ✨ <superuser.ntsystems@outlook.com>
2024-04-28 13:42:00 +0530

    Refresh Changes.(#37)
    Co-authored-by: node_module [bot] <node_modules@hotmail.com>

--- shell/macchanger.sh
+++ shell/macchanger.sh
@@ -1,7 +1,33 @@
-echo -e "\n\t Macchanger Script \n\t"
-echo -e "\t ===================== \t"
+#!/bin/bash
+echo -e "\n\t Device MAC Changer Script !\n"
+echo -e "Checking is macchanger is installed or not."
 sleep 1
 
-echo -e "Please install macchanger to continue, if already installed then continue.\n"
+if ! command -v macchanger &> /dev/null; then
+    echo "macchanger is not installed. Installing..."
+
+    sudo apt-get install macchanger
+
+    if ! command -v macchanger &> /dev/null; then
+        echo "Failed to install macchanger. Exiting."
+        exit 1
+    fi
+
+    echo "macchanger installed successfully."
+else
+    echo "macchanger is already installed."
+fi
+
+sudo macchanger -e eth0 --random
+echo -e "\vProcessing... 1st\n"
+sleep 2
+
 macchanger -e eth0 --random
-sleep 1
+echo -e "\vProcessing... 2nd\n"
+sleep 2
+
+macchanger -e eth0 --random
+echo -e "\vProcessing... 3rd\n"
+sleep 2
+
+echo -e "\n\t Device MAC Changed !\n"
